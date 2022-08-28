fcd271fa687f855db3734dc6a33ae650a4a2336c
prvladko <prvladko@gmail.com>
2022-08-28 11:17:17 +0300

    create Robot & RobotTest modules

---
+++ test/robot_test.exs
@@ -0,0 +1,9 @@
+defmodule ToyRobot.RobotTest do
+  @moduledoc """
+  Do we choose to write the tests in this file,
+  or would we write some doctests within the
+  ToyRobot.Robot module.
+  """
+  use ExUnit.Case
+  doctest ToyRobot.Robot
+end
